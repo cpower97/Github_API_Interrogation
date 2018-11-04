ca6f68dbcafa1a4187fd5153c9f93de40081b215
cpower97 <35492377+cpower97@users.noreply.github.com>
2018-11-04 20:56:40 +0000

    Converted JSON data of my account into R object

---
+++ User_Data_Display.R
@@ -0,0 +1,39 @@
+#Method sourced from https://gist.github.com/mGalarnyk/533ccfd1bb46680925448774bcb71647#file-r_github_api-r
+
+#install.packages("jsonlite")
+library(jsonlite)
+#install.packages("httpuv")
+library(httpuv)
+#install.packages("httr")
+library(httr)
+
+# Can be github, linkedin etc depending on application
+oauth_endpoints("github")
+
+# Change based on what you 
+myapp <- oauth_app(appname = "API_Interrogation",
+                   key = "640421140be54901944a",
+                   secret = "d0cb00d937e806396903d9c6ac980bacb846a61e")
+
+# Get OAuth credentials
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+
+# Use API
+gtoken <- config(token = github_token)
+req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+
+# Take action on http error
+stop_for_status(req)
+
+# Extract content from a request
+json1 = content(req)
+
+# Convert to a data.frame
+gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
+
+# Subset data.frame
+gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"] 
+
+
+userData = fromJSON("https://api.github.com/users/cpower97")
+userData
